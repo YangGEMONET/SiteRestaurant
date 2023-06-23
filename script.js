afae1fe68f4da49c817db17ff315d9e517ee7165
YangGEMONET <sophieyang@live.fr>
2023-06-23 18:55:17 +0200

    vérification des champs connexion

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-var login = document.getElementById('login')
-var mdp = document.getElementById('mdp')
-
-
-
-login.addEventListener('input', (event) => {
-  var loginValue = event.target.value
-  if (loginValue.lenght = 0 ){
-    alert("Veuillez renseigner le login") ;
-    console.log('Veuillez renseigner le login');
-  } 
-  else {
-    var veriflogin =  loginValue.indexof("@");
-    if (veriflogin == -1){
-        console.log('Le format de login nest pas conforme.');
-    }
-  }
-})
+document.forms["inscription"].addEventListener("submit", function(e) {
+ 
+	var erreur;
+ 
+	var inputs = this;
+ 
+	// Traitement générique
+	for (var i = 0; i < inputs.length; i++) {
+		if (!inputs[i].value) {
+			erreur = "Veuillez renseigner tous les champs";
+			break;
+		}
+	}
+ 
+	if (erreur) {
+		e.preventDefault();
+		document.getElementById("erreur").innerHTML = erreur;
+		return false;
+	} else {
+		alert('Formulaire envoyé !');
+	}
+	
+});
